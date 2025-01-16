f540fa39e33b39679e192e08de50ac7b7b4bf201
shaheem <shaheemmahmood728@gmail.com>
2025-01-16 16:31:30 +0530

    modified host permission

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['*.vercel.app','localhost']
+ALLOWED_HOSTS = ['*.vercel.app','localhost','*']
 
 # Application definition
 
