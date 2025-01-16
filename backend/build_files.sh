c61d92aa5b6d1df341eed0d0c0ab7e982208b625
shaheem <shaheemmahmood728@gmail.com>
2025-01-16 17:21:03 +0530

    `Update settings.py to allow .now.sh domains, update build script and vercel.json for static files and rewrites.`

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-echo "Building the project..."
+echo "BUILD START"
 python3.12 pip install -r requirements.txt
-
-echo "Make Migrations..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
-
-echo "Collect Static..."
 python3.12 manage.py collectstatic --noinput --clear
+echo "BUILD END"
